e292f9db1fcf627afc773a16ee2d81a0fb0392be
Jon <jon.a.mccallum@gmail.com>
2021-01-02 16:31:38 +0000

    change port again

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server running on 3000');
 });
